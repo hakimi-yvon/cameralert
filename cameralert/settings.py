7039e6c23fb459a21421679b30950b0678638363
hakimi-yvon <yvonndouanla@gmail.com>
2026-09-20 15:15:48 +0100

    Fix static files: alignement STATIC_ROOT avec staticfiles_build/static et distDir Vercel

--- cameralert/settings.py
+++ cameralert/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 # Media files
 MEDIA_URL = '/media/'
